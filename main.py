7109a1275eb1255f5ebc2fb60711d2fd2c77f09a
Atif-2602 <atifq2602@eng.rizvi.edu.in>
2026-03-01 15:00:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-# AIM: Develop a Python program that reads a text file and 
-# prints words of specified lengths (e.g., three, four, 
+# AIM: Develop a Python program that reads a text file and
+# prints words of specified lengths (e.g., three, four,
 # five, etc.) found within the file.
-# Coder:
-# Date:
+# Coder:Qureshi Atif
 
 print("--- Extracting Words from Text File ---\n")
+with open("story.txt") as f:
+    content=f.read().split()
 
+length=int(input("Enter Length of Words:"))
+
+words=[]
+for i in content:
+    if(len(i)==length):
+        words.append(i.lower())
+
+words=set(words)
+words=list(words)
+words=sorted(words)
+print(words)
